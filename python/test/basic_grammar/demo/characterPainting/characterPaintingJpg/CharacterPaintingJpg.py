07273bf500700f3e907b736d1aac4f4493db7d75
gaomeng <gaomeng@fc18.com.cn>
2019-02-13 10:50:09 +0800

    图片转字符画

--- python/test/basic_grammar/demo/characterPainting/characterPaintingJpg/CharacterPaintingJpg.py
+++ python/test/basic_grammar/demo/characterPainting/characterPaintingJpg/CharacterPaintingJpg.py
@@ -12,26 +12,31 @@ file_name = 'test.jpg';
 # file_name = '啊.jpg';
 # file_name = '棒.jpg';
 # serarr=['@','-',' ']
-serarr=['$','@','B','%','8','&','W','M','#','*',
+serarr=['$','@','B','%','8','&','W','M','# ','*',
         'o','a','h','k','b','d','p','q','w','m','Z','O','0','Q','L','C','J','U','Y','X','z','c','v','u','n','x','r','j','f','t',
         '/','\\','|','(',')','1','{','}','[',']','?','-','_','+','~','<','>','i','!','l','I',';',':',',','\"','^','`','\'','.' ]
-# serarr=['@','#','$','%','&','?','*','o','/','{','[','(','|','!','^','~','-','_',':',';',',','.','`',' ']
+# serarr=['@','# ','$','%','&','?','*','o','/','{','[','(','|','!','^','~','-','_',':',';',',','.','`',' ']
 count=len(serarr)
 
 def toText(image_file):
-    image_file=image_file.convert("L")#转灰度
-    asd =''#储存字符串
-    for h in range(0,  image_file.size[1]):#h
-        for w in range(0, image_file.size[0]):#w
+    # 转灰度
+    image_file=image_file.convert("L")
+    asd =''# 储存字符串
+    # h
+    for h in range(0,  image_file.size[1]):
+        # w
+        for w in range(0, image_file.size[0]):
             gray =image_file.getpixel((w,h))
             asd=asd+serarr[int(gray/(255/(count-1)))]
         asd=asd+'\n'
     return asd
 
 def toText2(image_file):
-    asd =''#储存字符串
-    for h in range(0,  image_file.size[1]):#h
-        for w in range(0, image_file.size[0]):#w
+    asd =''# 储存字符串
+    # h
+    for h in range(0,  image_file.size[1]):
+        # w
+        for w in range(0, image_file.size[0]):
             r,g,b =image_file.getpixel((w,h))
             gray =int(r* 0.299+g* 0.587+b* 0.114)
             asd=asd+serarr[int(gray/(255/(count-1)))]
@@ -39,9 +44,11 @@ def toText2(image_file):
     return asd
 
 
-image_file = Image.open(file_name) # 打开图片
-image_file=image_file.resize((int(image_file.size[0]*0.8), int(image_file.size[1]*0.4)))#调整图片大小
-# image_file=image_file.resize((int(image_file.size[0]*0.9), int(image_file.size[1]*0.5)))#调整图片大小
+# 打开图片
+image_file = Image.open(file_name)
+# 调整图片大小
+image_file=image_file.resize((int(image_file.size[0]*0.8), int(image_file.size[1]*0.4)))
+# image_file=image_file.resize((int(image_file.size[0]*0.9), int(image_file.size[1]*0.5)))
 
 print(u'Info:',image_file.size[0],' ',image_file.size[1],' ',count)
 try:
