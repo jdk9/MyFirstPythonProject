cccb80317aabf8b47171584d5a147a1096976cb3
gaomeng <gaomeng@fc18.com.cn>
2019-07-02 09:39:11 +0800

    100天学会python

---
+++ python/python-100-days/$01_初识Python.py
@@ -0,0 +1,19 @@
+# https://github.com/jackfrued/Python-100-Days/blob/master/Day01-15/01.%E5%88%9D%E8%AF%86Python.md
+# 2019年7月2日09:33:04
+
+# python之禅
+# import this
+
+# 小孩子玩的绘制图形
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('red')
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.mainloop()
---
+++ python/python-100-days/readme.md
@@ -0,0 +1,2 @@
+#100天学好python
+> 原文链接: [Python-100-Days](https://github.com/jackfrued/Python-100-Days)
